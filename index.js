4041b40fd289c1177fa8ea0056348eba372c466e
aamartinez17 <114872080+aamartinez17@users.noreply.github.com>
2024-12-19 19:50:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ const server = http.createServer((req, res) => {
 
      else if (req.url==='/api')
       {
+        const filePath = path.join(__dirname, 'public', 'db.json');
+      console.log("File Path:", filePath); // Add this line
           fs.readFile(
               path.join(__dirname, 'public', 'db.json'),'utf-8',
                       (err, content) => {
